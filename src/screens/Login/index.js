9cd8af63b0cd08ee9b3539aab7bf5389a285253d
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-24 15:27:38 +0500

    --Commit: Login  screen added

---
+++ src/screens/Login/index.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    TouchableHighlight,
+    Image,
+    Alert
+} from 'react-native';
+import Images from '../../assets/Images';
+
+export default class LoginView extends Component {
+
+    constructor(props) {
+        super(props);
+        state = {
+            email: '',
+            password: '',
+        }
+    }
+
+    onClickListener = (viewId) => {
+        Alert.alert("Alert", "Button pressed " + viewId);
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.inputContainer}>
+                    <Image style={styles.inputIcon} source={Images.email} />
+                    <TextInput style={styles.inputs}
+                        placeholder="Email"
+                        maxLength={30}
+                        keyboardType="email-address"
+                        underlineColorAndroid='transparent'
+                        onChangeText={(email) => this.setState({ email })} />
+                </View>
+
+                <View style={styles.inputContainer}>
+                    <Image style={styles.inputIcon} source={Images.password} />
+                    <TextInput style={styles.inputs}
+                        placeholder="Password"
+                        secureTextEntry={true}
+                        maxLength={30}
+                        underlineColorAndroid='transparent'
+                        onChangeText={(password) => this.setState({ password })} />
+                </View>
+
+                <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={() => this.onClickListener('login')}>
+                    <Text style={styles.loginText}>Login</Text>
+                </TouchableHighlight>
+
+                <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('restore_password')}>
+                    <Text>Forgot your password?</Text>
+                </TouchableHighlight>
+
+                <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('register')}>
+                    <Text>Register</Text>
+                </TouchableHighlight>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#DCDCDC',
+    },
+    inputContainer: {
+        borderBottomColor: '#F5FCFF',
+        backgroundColor: '#FFFFFF',
+        borderRadius: 30,
+        borderBottomWidth: 1,
+        width: 280,
+        height: 45,
+        marginBottom: 20,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    inputs: {
+        height: 45,
+        marginLeft: 16,
+        borderBottomColor: '#FFFFFF',
+        flex: 1,
+    },
+    inputIcon: {
+        width: 30,
+        height: 30,
+        marginLeft: 15,
+        justifyContent: 'center'
+    },
+    buttonContainer: {
+        height: 45,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
+        width: 280,
+        borderRadius: 30,
+    },
+    loginButton: {
+        backgroundColor: "#00b5ec",
+    },
+    loginText: {
+        color: 'white',
+    }
+});

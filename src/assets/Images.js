7156a16fb352d35adfbf7d76ba11836137afba3a
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-24 15:27:08 +0500

    --Commit: Icons added for login screen

---
+++ src/assets/Images.js
@@ -0,0 +1,6 @@
+const Images = {
+    password: { uri: 'https://png.icons8.com/key-2/ultraviolet/50/3498db' },
+    email: { uri: 'https://png.icons8.com/message/ultraviolet/50/3498db' },
+}
+
+export default Images 

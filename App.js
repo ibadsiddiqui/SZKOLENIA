6efa5c5113c7bb4ac1156a924378fba5967cf793
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-24 23:39:36 +0500

    --Commit: Firebase added

--- App.js
+++ App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './src/navigation/AppNavigator';
+import firebase from 'firebase';
+import config from './src/constants/Firebase';
 
 export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
   };
+
+  componentDidMount() {
+    firebase.initializeApp(config);
+  }
 
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
---
+++ src/constants/Firebase.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyCwFZ1vHAcTGB8YrqqEBFAYVO8mT2LWVhU",
+    authDomain: "loginsignupnative.firebaseapp.com",
+    databaseURL: "https://loginsignupnative.firebaseio.com",
+    projectId: "loginsignupnative",
+    storageBucket: "loginsignupnative.appspot.com",
+    messagingSenderId: "331903238162",
+    appId: "1:331903238162:web:a1e2a0262cafc8f8"
+};
+
+export default config;

b015d42ca52bb2f7b6554eb5020bd3241d1ed6b7
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-26 10:32:58 +0500

    --Commit: User service for creating in DB added

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,17 +1,17 @@
 import * as firebase from "firebase";
+import UserService from "./UserService";
 
 export default class AuthServices {
-    static async registerUser(email, password) {
+    static async registerUser(email, password, name) {
         try {
             const response = await firebase.auth().createUserWithEmailAndPassword(email, password);
             const { additionalUserInfo, user } = response
             if (additionalUserInfo.isNewUser) {
                 console.log(user.refreshToken)
+                await UserService.createUserInDB(user, name)
             }
-        } catch (error) {
-
+        } catch (error) {   
             console.log(error);
-
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;

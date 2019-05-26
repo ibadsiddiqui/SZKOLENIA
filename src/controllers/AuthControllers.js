34d3dcc2b37141cebed63fd2e73cc68b2b195e12
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-26 23:54:34 +0500

    --Commit: toggler and navigator added

--- src/controllers/AuthControllers.js
+++ src/controllers/AuthControllers.js
@@ -1,11 +1,22 @@
 import AuthServices from "../services/AuthService";
-import {Alert} from 'react-native'
+import { Alert, ToastAndroid } from 'react-native'
 export default class AuthController {
-    static async hanldeRegistration(props) {
+    static async hanldeRegistration(props, helper) {
         const { email, password, confirmPassword, name } = props;
+        const { navigate, resetState, toggleLoader } = helper
         if (password === confirmPassword) {
-            await AuthServices.registerUser(email, password, name );
-        }else {
+            toggleLoader()
+            try {
+                const response = await AuthServices.registerUser(email, password, name);
+                if (response === true)
+                    return navigate('MainTabNavigator');
+            } catch (error) {
+                ToastAndroid.show(error, ToastAndroid.SHORT)
+                toggleLoader()
+                resetState();
+                return;
+            }
+        } else {
             return Alert.alert('', "Passwords does not match.")
         }
     }

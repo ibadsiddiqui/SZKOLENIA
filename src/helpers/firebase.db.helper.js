2f6fa157274683762eb957773f4c439cb3b13a18
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-28 20:33:05 +0500

    --Commit: expo notification added in db

--- src/helpers/firebase.db.helper.js
+++ src/helpers/firebase.db.helper.js
@@ -1,13 +1,16 @@
 import * as firebase from 'firebase';
+import { registerForPushNotificationsAsync } from './notification.helper';
 
 export default class DBHelper {
 
   static async createUser(firebaseData, name) {
     const { uid, email, refreshToken } = firebaseData;
+    const notification = await registerForPushNotificationsAsync();
     await firebase.database().ref('users/' + uid).set({
       name: name,
       email: email,
       refreshToken: refreshToken,
+      notificationToken: notification,
       currentWordReceived: {},
       listOfWordsReceived: [],
     });

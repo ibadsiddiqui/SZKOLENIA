b4c59963e90be3f0248eeb77e9847961147707a1
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-29 02:32:36 +0500

    --Commit: User Services added for fetching tokens and list of words;

---
+++ push_notificaiton_sender/services/UsersServices.js
@@ -0,0 +1,13 @@
+const firebase = require('firebase-admin');
+
+const UserHelpers = module.exports;
+const firebaseHelpers = require('./../helpers/firebase.helpers')
+module.exports.getUsersPushNotificationToken = async () => {
+    const notificationTokens = await firebaseHelpers.getUsersTokensUsingID();
+    return notificationTokens;
+}
+
+module.exports.listOfWords = async () => {
+    const list = await firebaseHelpers.getListOfWords();
+    return list;
+}

99ba3718db1e180d661ee920d933b3ab8f55c791
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-29 02:31:37 +0500

    --commit: Firebase helper methods added

---
+++ push_notificaiton_sender/helpers/firebase.helpers.js
@@ -0,0 +1,39 @@
+const firebase = require('firebase-admin');
+const firebaseHelpers = module.exports;
+
+
+module.exports.getUsersTokensUsingID = async (ID) => {
+    const db = firebase.database()
+    const ref = db.ref('users/');
+    const userTokens = []
+    await ref.once('value', snapshot => {
+        snapshot.forEach(cs => {
+            userTokens.push(cs.val().notificationToken);
+        });
+    });
+    return userTokens;
+}
+
+module.exports.getUsersIDList = async (params) => {
+    const db = firebase.database()
+    const ref = db.ref('users/');
+    let results = []
+    await ref.once("value", snapshot => results.push(snapshot.val()));
+    console.log(results);
+
+    const UsersWithID = results.map((item) => Object.keys(item));
+    return UsersWithID[0]
+}
+
+
+module.exports.getListOfWords = async (params) => {
+    const db = firebase.database()
+    const ref = db.ref('Dictionary/');
+    const listOfWords = [];
+    await ref.once('value', snapshot => {
+        snapshot.forEach(cs => {
+            listOfWords.push(cs.val());
+        });
+    });
+    return listOfWords;
+}

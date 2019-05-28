b6e9df5e65d5029b76f395b4bbba5001c5c8aec4
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-29 02:32:05 +0500

    --Commit: Stack navigator added

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createStacreateStackNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
 import LoginView from '../screens/Login';
 import RegistrationView from '../screens/Registration';
 
-export default createAppContainer(createSwitchNavigator({
+export default createAppContainer(createStackNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Login: LoginView,

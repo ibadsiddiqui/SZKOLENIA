2783cbf5409afa7cab1afa920f32fb7efd70a002
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-29 09:07:28 +0500

    --Commit: notification field update function added

--- src/helpers/firebase.db.helper.js
+++ src/helpers/firebase.db.helper.js
@@ -5,15 +5,21 @@ export default class DBHelper {
 
   static async createUser(firebaseData, name) {
     const { uid, email, refreshToken } = firebaseData;
-    const notification = await registerForPushNotificationsAsync();
+    // const notification = await registerForPushNotificationsAsync();
     await firebase.database().ref('users/' + uid).set({
       name: name,
       email: email,
       refreshToken: refreshToken,
-      notificationToken: notification,
+      // notificationToken: notification,
       currentWordReceived: {},
       listOfWordsReceived: [],
     });
   }
-
+  static async updateUserToken(firebaseData) {
+    const { uid } = firebaseData;
+    const notification = await registerForPushNotificationsAsync();
+    await firebase.database().ref('users/' + uid).update({
+      notificationToken: notification,
+    });
+  }
 }

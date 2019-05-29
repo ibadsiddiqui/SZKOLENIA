2f067ba13bed90577ef629300c0c760dc7b7b9d9
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-30 00:52:52 +0500

    --commit: list view configured for words history

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,14 +1,80 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import { TouchableOpacity, View, Text, FlatList, AsyncStorage } from 'react-native';
+import * as firebase from "firebase";
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default class SettingsScreen extends React.Component {
-  static navigationOptions = {
-    title: 'app.json',
-  };
+  constructor() {
+    super();
+    this.state = {
+      receivedWords: []
+    }
+  }
+
+  async componentDidMount() {
+    console.log(this.state.receivedWords.length)
+    const uid = await AsyncStorage.getItem('uid');
+    const db = firebase.database();
+    await db.ref('/users/' + uid).on('value', (snapshot) => {
+      this.setState(snapshot.val());
+    })
+  }
+
+  renderRow = (item, key) => {
+    console.log(item);
+
+    return (
+      <TouchableOpacity key={key} style={{ marginLeft: 20, marginTop: 20 }}>
+        <View>
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={{ color: "black", fontSize: 15, marginTop: 5 }}>Word: </Text>
+            <Text style={{ color: "black", fontSize: 20, }}>{item.Word}</Text>
+          </View>
+          <View style={{ flexDirection: 'row', marginLeft: 20 }}>
+            <Text style={{ color: "black", fontSize: 13, }}>translation: </Text>
+            <Text style={{ color: "black", fontSize: 15, }}>{item.Translation}</Text>
+          </View>
+          <View style={{ flexDirection: 'row', marginLeft: 20 }}>
+            <Text style={{ color: "black", fontSize: 13, }}>meaning: </Text>
+            <Text style={{ color: "black", fontSize: 15, }}>{item.Meaning}</Text>
+          </View>
+          <View style={{ flexDirection: 'row', marginLeft: 20 }}>
+            <Text style={{ color: "black", fontSize: 13, }}>sentence: </Text>
+            <Text style={{ color: "black", fontSize: 15, }}>{item.Sentence}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  }
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <View>
+        <Text style={{ fontSize: 20, color: "#2e78b7", textAlign: 'center', marginTop: 40 }}>Previous Words:</Text>
+        <ScrollView automaticallyAdjustContentInsets={true} contentInsetAdjustmentBehavior='always'>
+          {
+            this.state.receivedWords.map((item, key) => (
+              <TouchableOpacity key={key} style={{ marginLeft: 20, marginTop: 20 }}>
+                <View>
+                  <Text style={{ color: "black", fontSize: 20, }}>{item.Word}</Text>
+                  <View style={{ flexDirection: 'row', marginLeft: 20, marginTop: 5 }}>
+                    <Text style={{ color: "black", fontSize: 13, marginTop: 2 }}>translation: </Text>
+                    <Text style={{ color: "black", fontSize: 15, width: 275 }}>{item.Translation}</Text>
+                  </View>
+                  <View style={{ flexDirection: 'row', marginLeft: 20, marginTop: 5 }}>
+                    <Text style={{ color: "black", fontSize: 13, marginTop: 5 }}>meaning: </Text>
+                    <Text style={{ color: "black", fontSize: 15, width: 275 }}>{item.Meaning}</Text>
+                  </View>
+                  <View style={{ flexDirection: 'row', marginLeft: 20, marginTop: 5 }}>
+                    <Text style={{ color: "black", fontSize: 13, marginTop: 5 }}>sentence: </Text>
+                    <Text style={{ color: "black", fontSize: 15, width: 275 }}>{item.Sentence}</Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            ))
+          }
+        </ScrollView>
+      </View>
+    );
   }
 }

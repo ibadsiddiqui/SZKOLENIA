805bce1514bb7b163fb71a5400abcf07c3cabea0
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-06-01 10:08:41 +0500

    --Commit: assets added for screens

--- src/assets/Images.js
+++ src/assets/Images.js
@@ -1,6 +1,8 @@
 const Images = {
     password: { uri: 'https://png.icons8.com/key-2/ultraviolet/50/3498db' },
     email: { uri: 'https://png.icons8.com/message/ultraviolet/50/3498db' },
+    background: require('./images/tree.jpg'),
+    backgroundLogin: require('./images/imae.jpeg')
 }
 
 export default Images 

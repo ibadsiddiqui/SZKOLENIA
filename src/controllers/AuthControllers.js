789e865150d7c70faca025ef9aafab0c869067ae
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-06-01 11:04:51 +0500

    --Commit: PIN check added for login

--- src/controllers/AuthControllers.js
+++ src/controllers/AuthControllers.js
@@ -1,41 +1,45 @@
 import AuthServices from "../services/AuthService";
 import { Alert, ToastAndroid } from 'react-native'
 export default class AuthController {
-    static async hanldeRegistration(props, helper) {
-        const { email, password, confirmPassword, name } = props;
-        const { navigate, resetState, toggleLoader } = helper
-        if (password === confirmPassword) {
-            toggleLoader()
-            try {
-                const response = await AuthServices.registerUser(email, password, name);
-                if (response === true)
-                    return navigate('Login');
-            } catch (error) {
-                ToastAndroid.show(error, ToastAndroid.SHORT)
-                toggleLoader()
-                resetState();
-                return;
-            }
-        } else {
-            return Alert.alert('', "Passwords does not match.")
-        }
-    }
+    // static async hanldeRegistration(props, helper) {
+    //     const { email, password, confirmPassword, name } = props;
+    //     const { navigate, resetState, toggleLoader } = helper
+    //     if (password === confirmPassword) {
+    //         toggleLoader()
+    //         try {
+    //             const response = await AuthServices.registerUser(email, password, name);
+    //             if (response === true)
+    //                 return navigate('Login');
+    //         } catch (error) {
+    //             ToastAndroid.show(error, ToastAndroid.SHORT)
+    //             toggleLoader()
+    //             resetState();
+    //             return;
+    //         }
+    //     } else {
+    //         return Alert.alert('', "Passwords does not match.")
+    //     }
+    // }
 
     static async hanldeSignIn(props, helper) {
         const { email, password } = props;
         const { navigate, resetState, toggleLoader } = helper
-        toggleLoader()
-        try {
-            const response = await AuthServices.signInUser(email, password);
-            if (response === true) {
-                toggleLoader()
-                return navigate('Main');
-            }
-        } catch (error) {
-            ToastAndroid.show(error, ToastAndroid.SHORT)
+        if(password === "SZKO0110"){
             toggleLoader()
-            resetState();
-            return;
+            try {
+                const response = await AuthServices.signInUser(email);
+                if (response === true) {
+                    toggleLoader()
+                    return navigate('Main');
+                }
+            } catch (error) {
+               ToastAndroid.show(error, ToastAndroid.SHORT)
+                toggleLoader()
+                resetState();
+                return;
+            }
+        }else {
+                ToastAndroid.show("Incorrect PIN", ToastAndroid.LONG)
         }
     }
 }

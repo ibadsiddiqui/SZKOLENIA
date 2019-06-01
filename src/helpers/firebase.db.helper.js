dd6e683f5f7b4330c437bc0e9d6174b6d2edf70f
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-06-01 11:03:36 +0500

    --Commit: DB helper function for user creation updated with push notification token

--- src/helpers/firebase.db.helper.js
+++ src/helpers/firebase.db.helper.js
@@ -1,25 +1,17 @@
 import * as firebase from 'firebase';
 import { registerForPushNotificationsAsync } from './notification.helper';
 
+const uuidv1 = require('uuid/v1');
+
 export default class DBHelper {
 
-  static async createUser(firebaseData, name) {
-    const { uid, email, refreshToken } = firebaseData;
-    // const notification = await registerForPushNotificationsAsync();
-    await firebase.database().ref('users/' + uid).set({
-      name: name,
-      email: email,
-      refreshToken: refreshToken,
-      // notificationToken: notification,
-      currentWordReceived: {},
-      listOfWordsReceived: [],
-    });
-  }
-  static async updateUserToken(firebaseData) {
-    const { uid } = firebaseData;
+  static async createUser(email) {
+    const uid = uuidv1();
     const notification = await registerForPushNotificationsAsync();
-    await firebase.database().ref('users/' + uid).update({
+    await firebase.database().ref('users/' + uid).set({
+      email: email,
       notificationToken: notification,
     });
+    return uid;
   }
 }

952f9fc91363eb3d89450a8434a8f55c5a326a7a
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-06-01 11:05:06 +0500

    --Commit: User serivce for registration removed

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,28 +1,14 @@
-import * as firebase from "firebase";
 import UserService from "./UserService";
 import { AsyncStorage } from 'react-native'
 export default class AuthServices {
-    static async registerUser(email, password, name) {
-        try {
-            const response = await firebase.auth().createUserWithEmailAndPassword(email, password);
-            const { additionalUserInfo, user } = response
-            if (additionalUserInfo.isNewUser) {
-                await UserService.createUserInDB(user, name)
-                return true;
-            }
-        } catch (error) {
-            throw error.message;
-        }
-    }
 
-    static async signInUser(email, password) {
+    static async signInUser(email) {
         try {
-            const response = await firebase.auth().signInWithEmailAndPassword(email, password);
-            await UserService.updateUserTokenInDB(response.user)
-            await AsyncStorage.setItem('uid', response.user.uid)
+            const uid = await UserService.createUserInDB(email);
+            await AsyncStorage.setItem('uid', uid)
             return true;
         } catch (error) {
-            throw error.message;
+            throw error;
         }
     }
 }

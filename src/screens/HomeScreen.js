a74e540bcf03dea85b0dacdeee738cf0a9e677c2
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-06-01 10:09:18 +0500

    --Commit: Background and table view added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {
   Platform,
-  ScrollView,
+  Dimensions,
   StyleSheet,
+  Image,
   AsyncStorage,
   Text,
   View,
 } from 'react-native';
 import * as firebase from "firebase";
+import Images from '../assets/Images';
+const { height, width } = Dimensions.get('window')
 
 export default class HomeScreen extends React.Component {
   constructor() {
@@ -35,26 +38,45 @@ export default class HomeScreen extends React.Component {
     const { Word, Meaning, Sentence, Translation } = this.state.currentWord
     return (
       <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Text style={{ fontSize: 18, color: 'purple' }}>Today's Word: </Text>
-            <Text style={{ fontSize: 30, color: '#2e78b7' }}>{Word}</Text>
+        <Image source={Images.background} resizeMode="cover" style={{ position: 'absolute', width, height }} />
+        <View style={{ flex: 1 }} />
+        <View style={{ flex: 3, alignItems: 'center', height: height * 0.5, justifyContent: 'center', borderWidth: 1, borderColor: "rgb(208,229,255)", marginHorizontal: 5 }}>
+          <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', borderWidth: 1, borderColor: "rgb(208,229,255)", }}>
+            <View style={{ flex: 0.75, alignSelf: 'stretch', borderWidth: 1, backgroundColor: "rgb(241,248,255)", borderColor: "rgb(208,229,255)", justifyContent: 'center' }}>
+              <Text style={{ fontSize: 16, color: 'purple', paddingLeft: 10, textAlignVertical: 'center' }}>Today's Word: </Text>
+            </View>
+            <View style={{ flex: 1.5, alignSelf: 'stretch', borderWidth: 1, backgroundColor: "rgb(241,248,255)", borderColor: "rgb(208,229,255)", justifyContent: 'center' }}>
+              <Text style={{ fontSize: 17, color: '#2e78b7', marginLeft: 5, }}>{Word}</Text>
+            </View>
           </View>
-          <View style={styles.welcomeContainer}>
-            <Text style={{ fontSize: 18, color: 'purple' }}>Translation: </Text>
-            <Text style={{ fontSize: 30, color: '#2e78b7' }}>{Translation}</Text>
+          <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', borderWidth: 1, borderColor: "rgb(208,229,255)", }}>
+            <View style={{ flex: 0.75, alignSelf: 'stretch', borderWidth: 1, backgroundColor: "rgb(241,248,255)", borderColor: "rgb(208,229,255)", justifyContent: 'center' }}>
+              <Text style={{ fontSize: 16, color: 'purple', paddingLeft: 10 }}>Translation: </Text>
+            </View>
+            <View style={{ flex: 1.5, alignSelf: 'stretch', borderWidth: 1, backgroundColor: "rgb(241,248,255)", borderColor: "rgb(208,229,255)", justifyContent: 'center' }}>
+              <Text style={{ fontSize: 17, color: '#2e78b7', marginLeft: 5, }}>{Translation}</Text>
+            </View>
           </View>
-          <View style={styles.welcomeContainer}>
-            <Text style={{ fontSize: 18, color: 'purple' }}>Meaning: </Text>
-            <Text style={{ fontSize: 20, color: '#2e78b7', textAlign: 'center', width: 250 }}>{Meaning}</Text>
+          <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', borderWidth: 1, borderColor: "rgb(208,229,255)", }}>
+            <View style={{ flex: 0.75, alignSelf: 'stretch', borderWidth: 1, backgroundColor: "rgb(241,248,255)", borderColor: "rgb(208,229,255)", justifyContent: 'center' }}>
+              <Text style={{ fontSize: 16, color: 'purple', paddingLeft: 10 }}>Meaning: </Text>
+            </View>
+            <View style={{ flex: 1.5, alignSelf: 'stretch', borderWidth: 1, backgroundColor: "rgb(241,248,255)", borderColor: "rgb(208,229,255)", justifyContent: 'center' }}>
+              <Text style={{ fontSize: 17, color: '#2e78b7', marginLeft: 5, }}>{Meaning}</Text>
+            </View>
           </View>
-          <View style={styles.welcomeContainer}>
-            <Text style={{ fontSize: 18, color: 'purple' }}>Sentence: </Text>
-            <Text style={{ fontSize: 20, color: '#2e78b7', textAlign: 'center', width: 250 }}>{Sentence}</Text>
+          <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', borderWidth: 1, borderColor: "rgb(208,229,255)", }}>
+            <View style={{ flex: 0.75, alignSelf: 'stretch', borderWidth: 1, backgroundColor: "rgb(241,248,255)", borderColor: "rgb(208,229,255)", justifyContent: 'center' }}>
+              <Text style={{ fontSize: 16, color: 'purple', paddingLeft: 10 }}>Sentence: </Text>
+            </View>
+            <View style={{ flex: 1.5, alignSelf: 'stretch', borderWidth: 1, backgroundColor: "rgb(241,248,255)", borderColor: "rgb(208,229,255)", justifyContent: 'center' }}>
+              <Text style={{ fontSize: 17, color: '#2e78b7', marginLeft: 5, }}>{Sentence}</Text>
+            </View>
           </View>
+        </View>
+        <View style={{ flex: 1 }} />
 
-        </ScrollView>
-      </View>
+      </View >
     );
   }
 }
@@ -63,7 +85,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 40
+    paddingTop: 40,
+    justifyContent: 'center'
   },
   developmentModeText: {
     marginBottom: 20,

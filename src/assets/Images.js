588ebcac75b211746a0abf0d72a619c85a43dbcb
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-06-09 18:29:03 +0500

    --Commit: new asset added

--- src/assets/Images.js
+++ src/assets/Images.js
@@ -1,7 +1,7 @@
 const Images = {
     password: { uri: 'https://png.icons8.com/key-2/ultraviolet/50/3498db' },
     username: { uri: 'https://img.icons8.com/ultraviolet/40/000000/gender-neutral-user.png' },
-    background: require('./images/tree.jpg'),
+    background: require('./images/background.jpeg'),
     backgroundLogin: require('./images/imae.jpeg')
 }
 
